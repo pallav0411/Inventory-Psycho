daae516ebe7abd79dc0d63d66e37253dbba67780
pallav0411 <84866942+pallav0411@users.noreply.github.com>
2023-05-07 15:11:09 +0530

    Add files via upload

---
+++ greaterwms/asgi.py
@@ -0,0 +1,17 @@
+import os
+
+from django.core.asgi import get_asgi_application
+from utils.websocket import websocket_application
+from asgihandler.core import ASGIHandler
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greaterwms.settings')
+
+http_application = get_asgi_application()
+
+async def application(scope, receive, send):
+    if scope['type'] in ['http', 'https']:
+        ASGIHandler.asgi_get_handler(scope)
+    elif scope['type'] in ['websocket']:
+        await websocket_application(scope, receive, send)
+    else:
+        raise Exception('Unknown Type' + scope['type'])
+
